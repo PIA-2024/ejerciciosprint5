d965320bc5ab8cfd37aaa7fab8342d194dd446f6
PIA-2024 <piaalbinamura@gmail.com>
2024-05-19 11:07:36 -0400

    Delete cpus.py

--- cpus.py
+++
@@ -1,4 +0,0 @@
-import os # importar la librería os
-
-cpu_num = os.cpu_count() # obtener el número de CPU en el sistema
-print(f"Mi computadora tiene {cpu_num} núcleos de CPU") # imprimir el número de CPU
